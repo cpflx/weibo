13ad98b7da126ab39674b0896b11bbcbc6a25d43
cpflx <547433687@qq.com>
2022-03-30 17:58:54 +0800

    注册页

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('signup', 'UsersController@create')->name('signup');
