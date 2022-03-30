5c93f5f1f8dd50ad1eef984e6559d1511e18c642
cpflx <547433687@qq.com>
2022-03-30 16:32:19 +0800

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>@yield('title', 'Weibo App')</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 
