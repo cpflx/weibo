cdb9bf309a304a71d733e1124a7b7ca8d122dabe
cpflx <547433687@qq.com>
2022-04-19 13:49:02 +0800

    中文的错误提示

---
+++ lang/zh_CN/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'custom' => [
+        'email' => [
+            'required' => '邮箱地址不能为空！',
+        ],
+    ],
+
+    'attributes' => [
+        'name' => '名称',
+        'email' => '邮箱',
+        'password' => '密码',
+    ],
+
+
+];
