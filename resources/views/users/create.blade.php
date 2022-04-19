659e4c66ad0ace81f4ac6d8c3ff6a66a18b069a4
cpflx <547433687@qq.com>
2022-04-19 10:47:51 +0800

    增加 CSRF 验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,6 +9,7 @@
       </div>
       <div class="card-body">
         <form method="POST" action="{{ route('users.store') }}">
+          {{ csrf_field() }}
           <div class="mb-3">
             <label for="name">名称：</label>
             <input type="text" name="name" class="form-control" value="{{ old('name') }}">
