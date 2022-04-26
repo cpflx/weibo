4cfc708773597e63a4563d8068cbd68a20a811a9
cpflx <547433687@qq.com>
2022-04-26 15:01:54 +0800

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -29,8 +29,12 @@ class StatusesController extends Controller
 
     }
 
-    public function destroy()
+    public function destroy(Status $status)
     {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
+        return redirect()->back();
 
     }
 
